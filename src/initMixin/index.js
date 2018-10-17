22941d18980050ae52dadb5b14ffeff612a32dac
weiwang <wangweile@shuidihuzhu.com>
2018-10-17 23:09:50 +0800

    去掉依赖

--- src/initMixin/index.js
+++ src/initMixin/index.js
@@ -1,10 +1,15 @@
 import {initState} from "../initState";
+import {mergeOptions} from "../util/options";
 
 export function initMixin(Vue) {
     Vue.prototype._init = function (options) { // options上层传过来的vue对象
         const vm = this
         vm.$options = options
         initState(vm)
-        console.log('vm', vm)
+
+        if(vm.$options.el){
+            console.log(vm.$options.el)
+        }
+        mergeOptions()
     }
 }
